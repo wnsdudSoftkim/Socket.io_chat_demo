53c80a5e0daf536870639b664f34742b00a8b809
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-02-22 19:51:20 +0900

    test : socket.io demo  * Learn realtime communication

--- index.js
+++ index.js
@@ -5,71 +5,30 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
+
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Chatroom
+//socket이 파라미터로 전달 된 후 
 
-let numUsers = 0;
+var count = 1
+io.on('connection',(socket)=> {
+  console.log("user connected : ",socket.id)
+  const name ="user " + count++;
+  io.to(socket.id).emit('change name',name)
 
-io.on('connection', (socket) => {
-  let addedUser = false;
+  socket.on('disconnect',()=> {
+    console.log('user disconnected : ',socket.id)
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
+  })
+  //emit을 통해 모든 클라이언트들에게 msg를 전달한다
+  socket.on('send message',(name,text)=> {
+    const msg = name+":"+text
+    console.log(msg)
+    io.emit('receive message',msg)
+  })
+})
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
